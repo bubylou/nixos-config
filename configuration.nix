a4ed8f344001841c909eb27a13e89befc1356a0f
Nicholas Malcolm <bubylou@pm.me>
2025-10-26 20:58:41 -0400

    initial

---
+++ configuration.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ./disk-config.nix ./hardware-configuration.nix ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nas02";
+  time.timeZone = "America/New_York";
+
+  users.users.buby = {
+    isNormalUser = true;
+    description = "Nicholas Malcolm";
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLpbx2wvIh7+iksunMRtHh9qDfwojF4j/ObtH+IdxMD buby@stealth16ai"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhoQaZW/NXiN5504yj83XwTB+lzgwmvKnMq2LMQyYh9 buby@xps13-9370"
+    ];
+  };
+
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
+
+  security.sudo.extraRules = [{
+    groups = [ "wheel" ];
+    commands = [{
+      command = "ALL";
+      options = [ "NOPASSWD" ];
+    }];
+  }];
+
+  environment.systemPackages = with pkgs; [ curl gitMinimal ];
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "25.05";
+}
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, disko, ... }: {
+    nixosConfigurations.nas02 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
