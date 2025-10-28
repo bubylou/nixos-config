f6587ecc25272972535c07c399ecf4b94138e97a
Nicholas Malcolm <bubylou@pm.me>
2025-10-28 16:17:23 -0400

    add terible user

--- machines/common/default.nix
+++ machines/common/default.nix
@@ -5,14 +5,22 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   time.timeZone = "America/New_York";
-  users.users.buby = {
-    isNormalUser = true;
-    description = "Nicholas Malcolm";
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLpbx2wvIh7+iksunMRtHh9qDfwojF4j/ObtH+IdxMD buby@stealth16ai"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhoQaZW/NXiN5504yj83XwTB+lzgwmvKnMq2LMQyYh9 buby@xps13-9370"
-    ];
+  users.users = {
+    buby = {
+      isNormalUser = true;
+      description = "Nicholas Malcolm";
+      extraGroups = [ "networkmanager" "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLpbx2wvIh7+iksunMRtHh9qDfwojF4j/ObtH+IdxMD buby@stealth16ai"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhoQaZW/NXiN5504yj83XwTB+lzgwmvKnMq2LMQyYh9 buby@xps13-9370"
+      ];
+    };
+
+    terible = {
+      isNormalUser = true;
+      description = "Teri Malcolm";
+      extraGroups = [ "networkmanager" ];
+    };
   };
 
   services.tailscale.enable = true;
