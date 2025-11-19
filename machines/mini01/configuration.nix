501f20d924481b1800bb9be5872ab69e94421371
Nicholas Malcolm <bubylou@pm.me>
2025-11-18 20:59:03 -0500

    pipewire bluetooth all devices
    Create pipewire option to send audo to all bluetooth devices.

--- machines/mini01/configuration.nix
+++ machines/mini01/configuration.nix
@@ -21,7 +21,41 @@
     };
   };
 
-  networking.interfaces.wlp0s20f3.wakeOnLan.enable = true;
+  services.pipewire.configPackages = [
+    (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-bluetooth-all.conf" ''
+      context.modules = [
+        {   name = libpipewire-module-combine-stream
+            args = {
+                combine.mode = sink
+                node.name = "bt-all"
+                node.description = "bluetooth broadcast to all devices"
+                combine.latency-compensate = false
+                combine.props = {
+                    audio.position = [ FL FR ]
+                }
+                stream.props = {
+                }
+                stream.rules = [
+                    {
+                        matches = [
+                            {
+                                node.name = "~bluez_output.*"
+                                media.class = "Audio/Sink"
+                            }
+                        ]
+                        actions = {
+                            create-stream = {
+                                combine.audio.position = [ FL FR ]
+                                audio.position = [ FL FR ]
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+        ]
+    '')
+  ];
 
   system.stateVersion = "25.05";
 }
