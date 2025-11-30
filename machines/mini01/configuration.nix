9349e7c4202d8caab0473b9c6488b49d100b3b14
Nicholas Malcolm <bubylou@pm.me>
2025-11-30 12:48:27 -0500

    mDNS device discovery
    For remote apps trying to find kodi

--- machines/mini01/configuration.nix
+++ machines/mini01/configuration.nix
@@ -21,41 +21,49 @@
     };
   };
 
-  services.pipewire.configPackages = [
-    (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-bluetooth-all.conf" ''
-      context.modules = [
-        {   name = libpipewire-module-combine-stream
-            args = {
-                combine.mode = sink
-                node.name = "bt-all"
-                node.description = "bluetooth broadcast to all devices"
-                combine.latency-compensate = false
-                combine.props = {
-                    audio.position = [ FL FR ]
+  services = {
+    pipewire.configPackages = [
+      (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-bluetooth-all.conf" ''
+        context.modules = [{
+          name = libpipewire-module-combine-stream
+          args = {
+            combine.mode = sink
+            node.name = "bt-all"
+            node.description = "bluetooth broadcast to all devices"
+            combine.latency-compensate = false
+            combine.props = { audio.position = [ FL FR ] }
+            stream.props = {}
+            stream.rules = [{
+              matches = [
+                {
+                  node.name = "~bluez_output.*"
+                  media.class = "Audio/Sink"
                 }
-                stream.props = {
+              ]
+              actions = {
+                create-stream = {
+                  combine.audio.position = [ FL FR ]
+                  audio.position = [ FL FR ]
                 }
-                stream.rules = [
-                    {
-                        matches = [
-                            {
-                                node.name = "~bluez_output.*"
-                                media.class = "Audio/Sink"
-                            }
-                        ]
-                        actions = {
-                            create-stream = {
-                                combine.audio.position = [ FL FR ]
-                                audio.position = [ FL FR ]
-                            }
-                        }
-                    }
-                ]
-            }
-        }
-        ]
-    '')
-  ];
+              }
+            }]
+          }
+        }]
+      '')
+    ];
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        workstation = true;
+        userServices = true;
+      };
+    };
+  };
 
   system.stateVersion = "25.05";
 }
