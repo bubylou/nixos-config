04a751e9ee4398e0c0cf34ad3116bcb2fa3c9cf8
Nicholas Malcolm <bubylou@pm.me>
2025-12-16 23:25:55 -0500

    move shared desktop settings

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,33 +1,3 @@
 {...}: {
   imports = [./gnome.nix ./hyprland.nix ./kde.nix ./kodi.nix];
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  services = {
-    xserver.xkb.layout = "us";
-    xserver.xkb.variant = "";
-
-    printing.enable = true;
-
-    pulseaudio.enable = false;
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
-  };
-
-  security.rtkit.enable = true;
 }
