4ae0c9fb895b5ad134d664f3b8165a3ea89eccca
Nicholas Malcolm <bubylou@pm.me>
2025-12-23 14:21:09 -0500

    swap jf from container to nixos service

--- modules/home-lab/jellyfin.nix
+++ modules/home-lab/jellyfin.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -8,60 +9,55 @@ in {
   options.home-lab.jellyfin = {
     enable = lib.mkEnableOption "enables jellyfin server";
 
-    host = lib.mkOption {
+    domain = lib.mkOption {
+      type = lib.types.str;
+      default = "jellyfin.${config.home-lab.domain}";
+      example = "jellyfin.example.com";
+    };
+
+    address = lib.mkOption {
       type = lib.types.str;
       default = "127.0.0.1";
-      example = "127.0.0.1";
+      example = "0.0.0.0";
     };
 
     port = lib.mkOption {
       type = lib.types.int;
       default = 8096;
-      example = 8096;
-    };
-
-    volumes = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = ["jellyfin_data:/config"];
-      example = ["jellyfin_data:/config" "/mnt/nfs:/movies"];
+      example = 443;
     };
   };
 
   config = lib.mkIf cfg.enable {
-    virtualisation.oci-containers.containers = {
+    services = {
       jellyfin = {
-        image = "ghcr.io/linuxserver/jellyfin:10.11.5";
-        extraOptions = ["--device" "nvidia.com/gpu=all"];
-        environment = {
-          TZ = "America/New_York";
-          PUID = "1000";
-          GUID = "100";
+        enable = true;
+      };
+
+      caddy = {
+        virtualHosts."${cfg.domain}" = {
+          useACMEHost = config.home-lab.domain;
+          extraConfig = ''
+            import auth
+            reverse_proxy http://${cfg.address}:${toString cfg.port}
+          '';
         };
-        ports = [
-          "${toString cfg.port}:8096"
-        ];
-        inherit (cfg) volumes;
       };
-    };
 
-    services.caddy = {
-      virtualHosts."jellyfin.${config.home-lab.domain}" = {
-        useACMEHost = config.home-lab.domain;
-        extraConfig = ''
-          import auth
-          reverse_proxy http://${cfg.host}:${toString cfg.port}
-        '';
-      };
+      gatus.settings.endpoints = [
+        {
+          name = "jellyfin";
+          url = "http://${cfg.address}:${toString cfg.port}";
+          interval = "1m";
+          client.dns-resolver = "tcp://127.0.0.1:53";
+          conditions = ["[STATUS] == 200" "[RESPONSE_TIME] < 100"];
+        }
+      ];
     };
-
-    services.gatus.settings.endpoints = [
-      {
-        name = "jellyfin";
-        url = "http://${cfg.host}:${toString cfg.port}";
-        interval = "1m";
-        client.dns-resolver = "tcp://127.0.0.1:53";
-        conditions = ["[STATUS] == 200" "[RESPONSE_TIME] < 100"];
-      }
+    environment.systemPackages = with pkgs; [
+      jellyfin
+      jellyfin-web
+      jellyfin-ffmpeg
     ];
   };
 }
