6382acfa52f6bbce079ba3a475e837f63391e414
Nicholas Malcolm <bubylou@pm.me>
2025-12-30 21:00:44 -0500

    sonarr and prowlarr

--- modules/home-lab/prowlarr.nix
+++ modules/home-lab/prowlarr.nix
@@ -5,75 +5,15 @@
 }: let
   cfg = config.home-lab.prowlarr;
 in {
-  options.home-lab.prowlarr = {
-    enable = lib.mkEnableOption "enables prowlarr server";
-
-    disableAuth = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-    };
-
-    url = lib.mkOption {
-      type = lib.types.str;
-      default = "prowlarr.${config.home-lab.domain}";
-      example = "example.com";
-    };
-
-    address = lib.mkOption {
-      type = lib.types.str;
-      default = "127.0.0.1";
-      example = "0.0.0.0";
-    };
-
-    port = lib.mkOption {
-      type = lib.types.int;
-      default = 9696;
-      example = 443;
-    };
-  };
+  imports = [
+    (import ./common/arr.nix {
+      name = "prowlarr";
+      port = 9696;
+      inherit config lib;
+    })
+  ];
 
   config = lib.mkIf cfg.enable {
-    services = {
-      prowlarr = {
-        enable = true;
-
-        environmentFiles = [
-          "/run/keys/prowlarr-apikey"
-        ];
-
-        settings = {
-          server = {
-            bindaddress = cfg.address;
-            inherit (cfg) port;
-          };
-
-          auth = lib.mkIf cfg.disableAuth {
-            enabled = false;
-            method = "External";
-            authenticationrequired = false;
-          };
-        };
-      };
-
-      caddy = {
-        virtualHosts."${cfg.url}" = {
-          useACMEHost = config.home-lab.domain;
-          extraConfig = ''
-            import auth
-            reverse_proxy http://${cfg.address}:${toString cfg.port}
-          '';
-        };
-      };
-
-      gatus.settings.endpoints = [
-        {
-          name = "prowlarr";
-          url = "http://${cfg.address}:${toString cfg.port}";
-          interval = "1m";
-          client.dns-resolver = "tcp://127.0.0.1:53";
-          conditions = ["[STATUS] == 200" "[RESPONSE_TIME] < 100"];
-        }
-      ];
-    };
+    services.prowlarr.enable = true;
   };
 }
