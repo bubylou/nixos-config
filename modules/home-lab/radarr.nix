8aec89f20b04535fa972e64614cb3e932dea4bc9
Nicholas Malcolm <bubylou@pm.me>
2025-12-30 20:38:40 -0500

    radarr

--- modules/home-lab/radarr.nix
+++ modules/home-lab/radarr.nix
@@ -5,9 +5,19 @@
 }: let
   cfg = config.home-lab.radarr;
 in {
-  options.home-lab.radarr = {
-    enable = lib.mkEnableOption "enables radarr server";
+  imports = [
+    (import ./common/basic-options.nix {
+      name = "radarr";
+      port = 7878;
+      inherit config lib;
+    })
+    (import ./common/basic-config.nix {
+      name = "radarr";
+      inherit config lib;
+    })
+  ];
 
+  options.home-lab.radarr = {
     environmentFiles = lib.mkOption {
       type = lib.types.listOf lib.types.path;
       default = [
@@ -22,24 +32,7 @@ in {
     disableAuth = lib.mkOption {
       type = lib.types.bool;
       default = true;
-    };
-
-    url = lib.mkOption {
-      type = lib.types.str;
-      default = "radarr.${config.home-lab.domain}";
-      example = "example.com";
-    };
-
-    address = lib.mkOption {
-      type = lib.types.str;
-      default = "127.0.0.1";
-      example = "0.0.0.0";
-    };
-
-    port = lib.mkOption {
-      type = lib.types.int;
-      default = 7878;
-      example = 443;
+      example = false;
     };
   };
 
@@ -53,36 +46,16 @@ in {
         settings = {
           server = {
             bindaddress = cfg.address;
-            port = cfg.port;
+            inherit (cfg) port;
           };
 
-          auth = lib.mkIf (cfg.disableAuth) {
+          auth = lib.mkIf cfg.disableAuth {
             enabled = false;
             method = "External";
             authenticationrequired = false;
           };
         };
       };
-
-      caddy = {
-        virtualHosts."${cfg.url}" = {
-          useACMEHost = config.home-lab.domain;
-          extraConfig = ''
-            import auth
-            reverse_proxy http://${cfg.address}:${toString cfg.port}
-          '';
-        };
-      };
-
-      gatus.settings.endpoints = [
-        {
-          name = "radarr";
-          url = "http://${cfg.address}:${toString cfg.port}";
-          interval = "1m";
-          client.dns-resolver = "tcp://127.0.0.1:53";
-          conditions = ["[STATUS] == 200" "[RESPONSE_TIME] < 100"];
-        }
-      ];
     };
   };
 }
